ba535453d0c466058a2b9a23c1905091bbeef79b
Max McClintock <maxmcclintock@Maxs-MBP.lan>
2024-07-22 15:29:09 -0500

    UserServiceImpl class variables

--- backend/src/main/java/com/greenteam/FullStackApplication/services/impl/UserServiceImpl.java
+++ backend/src/main/java/com/greenteam/FullStackApplication/services/impl/UserServiceImpl.java
@@ -1,5 +1,8 @@
 package com.greenteam.FullStackApplication.services.impl;
 
+import com.greenteam.FullStackApplication.mappers.BasicUserMapper;
+import com.greenteam.FullStackApplication.mappers.FullUserMapper;
+import com.greenteam.FullStackApplication.repositories.UserRepository;
 import com.greenteam.FullStackApplication.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -7,4 +10,11 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
+
+    /* Mappers */
+    private final BasicUserMapper basicUserMapper;
+    private final FullUserMapper fullUserMapper;
+
+    /* Repositories */
+    private final UserRepository userRepository;
 }
