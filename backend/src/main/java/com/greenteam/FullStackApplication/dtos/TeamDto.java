1e8d9e33c3b64d83e17e552bd642dab1f56788ab
Jacob Taylor <jtlikesps4@icloud.com>
2024-07-22 13:22:54 -0500

    Added company,project,team,and user request dtos

---
+++ backend/src/main/java/com/greenteam/FullStackApplication/dtos/TeamDto.java
@@ -0,0 +1,18 @@
+package com.greenteam.FullStackApplication.dtos;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@NoArgsConstructor
+@Data
+public class TeamDto {
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private Set<BasicUserDto> teammates;
+}
