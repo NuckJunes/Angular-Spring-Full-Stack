85b0c84c0a1c27839946d53e24634bafe859ac81
Jacob Taylor <jtlikesps4@icloud.com>
2024-07-22 15:28:11 -0500

    added company services and controller

---
+++ backend/src/main/java/com/greenteam/FullStackApplication/services/impl/CompanyServiceImpl.java
@@ -0,0 +1,10 @@
+package com.greenteam.FullStackApplication.services.impl;
+
+import com.greenteam.FullStackApplication.services.CompanyService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CompanyServiceImpl implements CompanyService {
+}
