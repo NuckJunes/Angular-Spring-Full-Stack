db498ea8418d4eb4636d136b8c632808695b9467
Max McClintock <maxmcclintock@Maxs-MBP.lan>
2024-07-22 15:08:53 -0500

    Created AnnouncementsController, AnnouncementsService, and AnnouncementsServiceImpl

---
+++ backend/src/main/java/com/greenteam/FullStackApplication/services/impl/AnnouncementsServiceImpl.java
@@ -0,0 +1,10 @@
+package com.greenteam.FullStackApplication.services.impl;
+
+import com.greenteam.FullStackApplication.services.AnnouncementsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AnnouncementsServiceImpl implements AnnouncementsService {
+}
