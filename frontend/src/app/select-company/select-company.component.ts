b8cc9b2d5b9a0b5967e53af90a42886969544c20
Xindref <dakotahmccrary@gmail.com>
2024-07-22 12:11:10 -0500

    finished styling and setup of Announcement page

---
+++ frontend/src/app/select-company/select-company.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-select-company',
+  standalone: true,
+  imports: [],
+  templateUrl: './select-company.component.html',
+  styleUrl: './select-company.component.css'
+})
+export class SelectCompanyComponent {
+  
+}
