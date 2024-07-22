e7c0967a5dcf6f20722fe8a074040bd6ceddc48f
Max McClintock <maxmcclintock@Maxs-MBP.lan>
2024-07-22 14:58:00 -0500

    UserController, UserService, and UserServiceImpl created

---
+++ backend/src/main/java/com/greenteam/FullStackApplication/services/impl/UserServiceImpl.java
@@ -0,0 +1,10 @@
+package com.greenteam.FullStackApplication.services.impl;
+
+import com.greenteam.FullStackApplication.services.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+}
