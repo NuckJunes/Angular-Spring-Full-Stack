0ff782790ecd611308feafe76853f61b99942539
Nick Jones <jonesn5@miamioh.edu>
2024-07-22 14:08:00 -0400

    Added login variables and api call."

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { post } from '../../services/api';
 
 @Component({
   selector: 'app-login',
@@ -9,6 +10,16 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  email: String = "";
+  password: String = "";
+
   //Here we need to fetch a the user by username and password 
   //If that person exists, then we navigate to select company 
+
+  async login() {
+    //If we get a valid response, route to select company page
+    let response = post("users", ["login"], {username: this.password})
+    
+  }
+
 }
