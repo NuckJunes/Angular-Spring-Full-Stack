cbd95d18f8e1e5133a11b9fcff038d11ec3ef840
Nick Jones <jonesn5@miamioh.edu>
2024-07-22 12:17:53 -0400

    Added initial login and service file

---
+++ frontend/src/app/login/login.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+
+  //Here we need to fetch a the user by username and password 
+  //If that person exists, then we navigate to select company 
+}
