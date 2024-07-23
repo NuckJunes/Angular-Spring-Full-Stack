4143d9de496d6425211cd0366f9d2122f6de70dd
Xindref <dakotahmccrary@gmail.com>
2024-07-23 12:13:00 -0500

    added select-company styling and implementation with dummy data

--- frontend/src/app/select-company/select-company.component.ts
+++ frontend/src/app/select-company/select-company.component.ts
@@ -1,12 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-select-company',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './select-company.component.html',
   styleUrl: './select-company.component.css'
 })
 export class SelectCompanyComponent {
+
+  companies: string[] = ['FedEx', 'Cook Systems', 'Google'];
 
 }
