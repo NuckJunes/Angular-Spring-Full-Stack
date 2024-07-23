f989b847cf199a1190fce48035d4035e3970fda4
Xindref <dakotahmccrary@gmail.com>
2024-07-23 08:49:32 -0500

    fixed announcement component missing files and routes setup again

---
+++ frontend/src/app/announcements/announcements.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-projects',
+  standalone: true,
+  imports: [],
+  templateUrl: './announcements.component.html',
+  styleUrl: './announcements.component.css'
+})
+export class AnnouncementsComponent {
+
+}
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { AnnouncementsComponent } from './announcements/announcements.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { SelectCompanyComponent } from './select-company/select-company.component';
+import { TeamsComponent } from './teams/teams.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'login', component: LoginComponent},
+    {path: '', component: AnnouncementsComponent},
+    {path: 'projects', component: ProjectsComponent},
+    {path: 'select-company', component: SelectCompanyComponent},
+    {path: 'teams', component: TeamsComponent}
+];
