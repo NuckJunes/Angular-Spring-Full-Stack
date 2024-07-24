61af3cd0dc1602da3c0a4ea0aa027671c5a1250a
Nick Jones <jonesn5@miamioh.edu>
2024-07-24 15:14:23 -0400

    Added functionality for adding teams, including adding users to teams.

--- frontend/src/app/teams/teams-add-form/teams-add-form.component.ts
+++ frontend/src/app/teams/teams-add-form/teams-add-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, Inject, Injectable } from '@angular/core';
 import { NgFor } from '@angular/common';
 import { MatDialog, MAT_DIALOG_DATA, MatDialogRef, MatDialogTitle, MatDialogContent, MatDialogActions, MatDialogClose } from '@angular/material/dialog';
+import {MatSelectModule} from '@angular/material/select';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormField, MatInputModule } from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
@@ -12,7 +13,7 @@ import { userInfo } from '../../../services/userInfo';
 @Component({
   selector: 'app-teams-add-form',
   standalone: true,
-  imports: [NgFor, MatInputModule, FormsModule, MatButtonModule, MatFormField, MatDialogTitle, MatDialogClose, MatDialogContent, MatDialogActions],
+  imports: [NgFor, MatInputModule, FormsModule, MatButtonModule, MatFormField, MatDialogTitle, MatDialogClose, MatDialogContent, MatDialogActions, MatSelectModule],
   templateUrl: './teams-add-form.component.html',
   styleUrl: './teams-add-form.component.css'
 })
@@ -27,6 +28,23 @@ export class TeamsAddFormComponent {
 
   users: FullUserDTO[] = [];
 
+  allUsers: FullUserDTO[] = [];
+
+  currentUser: FullUserDTO = {
+    id: 0,
+    profile: {
+      firstname: "",
+      lastname: "",
+      email: "",
+      phone: ""
+    },
+    isAdmin: false,
+    active: false,
+    status: "",
+    companies: [],
+    teams: []
+  };
+
   constructor(private userInfo: userInfo, public dialogRef: MatDialogRef<TeamsAddFormComponent>, @Inject(MAT_DIALOG_DATA) public data: TeamDTO) {}
 
   ngOnInit() {
@@ -38,8 +56,31 @@ export class TeamsAddFormComponent {
     let id = this.userInfo.getCompanyID();
     let response = await get("company", [id.toString(), "users"])
     if(response) {
+      this.allUsers = response;
       this.users = response;
     }
+  }
+
+  addUser() {
+    //We must convert from FullUserDTO to BasicUserDTO
+    //Removes currentUser from users and adds to teamToAdd.users
+    let pos = this.users.indexOf(this.currentUser);
+    this.users.splice(pos, 1)
+    this.teamToAdd.users.push({
+      id: this.currentUser.id,
+      profile: this.currentUser.profile,
+      isAdmin: this.currentUser.isAdmin,
+      active: this.currentUser.active,
+      status: this.currentUser.status
+    });
+  }
+
+  removeUser(id: number) {
+    //We must convert from BasicUserDTO to FullUserDTO
+    //Removes user by id in the teamToAdd.users and add it to users
+    this.teamToAdd.users = this.teamToAdd.users.filter((user) => user.id !== id)
+    let tmp = this.allUsers.filter((user) => user.id === id)
+    this.users.push(tmp[0]);
   }
 
   onCancelClick() {
