2beff76372174af6e7fc1f6c8eea392b862dea48
Nick Jones <jonesn5@miamioh.edu>
2024-07-24 09:09:16 -0400

    Added some functionality to teams page.

--- frontend/src/app/teams/teams.component.ts
+++ frontend/src/app/teams/teams.component.ts
@@ -1,12 +1,42 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { userInfo } from '../../services/userInfo';
+import { get } from '../../services/api';
+import TeamDTO from '../models/TeamDTO';
+import { SingleTeamComponent } from "./single-team/single-team.component";
 
 @Component({
   selector: 'app-teams',
   standalone: true,
-  imports: [],
+  imports: [NgFor, SingleTeamComponent],
   templateUrl: './teams.component.html',
   styleUrl: './teams.component.css'
 })
 export class TeamsComponent {
 
+  id: string = "";
+  teams: TeamDTO[] = [];
+
+  constructor(private userInfo: userInfo) {}
+
+  ngOnInit() {
+    this.userInfo.getCompanyID().subscribe((value) => (this.id = value.toString()));
+    this.getTeams();
+  }
+
+  async getTeams() {
+    let response = await get("company", [this.id, "teams"]);
+    if(response) {
+      this.teams = response;
+    }
+  }
+
+  async loadProjects(teamId: number) {
+    let response = await get("company", [this.id, "teams", teamId.toString(), "projects"]);
+    if(response) {
+      return response.length();
+    }
+  }
+
+  
 }
