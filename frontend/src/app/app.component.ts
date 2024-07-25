271df24909952c1c7262e960ae30ced302e8a4ed
Nick Jones <jonesn5@miamioh.edu>
2024-07-25 10:56:44 -0400

    Added add team functionality, tested with DB.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AnnouncementsComponent } from "./announcements/announcements.component";
 
-
 @Component({
   selector: 'app-root',
   standalone: true,
